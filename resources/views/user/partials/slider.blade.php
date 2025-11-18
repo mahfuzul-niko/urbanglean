0dddf966de388d8bf6f9b00b105787339840a3e9
mahfuzulislam <mahfuzul1125@gmail.com>
2025-11-18 11:44:11 +0600

    done

--- resources/views/user/partials/slider.blade.php
+++ resources/views/user/partials/slider.blade.php
@@ -7,7 +7,7 @@
                 <div class="hero__slider--inner hero__slider--activation swiper">
                     <div class="hero__slider--wrapper swiper-wrapper">
                         @foreach($sliders as $slider) 
-                        <div class="swiper-slide ">
+                        <a href="{{optional($slider)->link}}" class="swiper-slide ">
                             <div class="hero__slider--items home1__slider--bg img-fluid" style="
                                 background:url('{{ asset('images/slider/'.$slider->image ) }}'); 
                                 ">
@@ -20,11 +20,11 @@
                                                         {{-- <img class="slider__text--shape__icon" src="assets/img/icon/text-shape-icon.png" alt="text-shape-icon"> --}} {{-- New Collection --}}</p> 
                                                     {{--<h2 class="slider__content--maintitle h1">{!! optional($slider)->title !!}</h2>--}}
                                                     {{--<p class="slider__content--desc desc2 d-sm-2-none mb-40">{!! optional($slider)->description !!}</p>--}}    
-                                                    <a class="slider__btn primary__btn" href="{{optional($slider)->link}}"> Show Collection
+                                                    {{-- <a class="slider__btn primary__btn" href="{{optional($slider)->link}}"> Show Collection
                                                         <svg class="primary__btn--arrow__icon" xmlns="http://www.w3.org/2000/svg" width="20.2" height="12.2" viewBox="0 0 6.2 6.2">
                                                         <path d="M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z" transform="translate(-4 -4)" fill="currentColor"></path>
                                                         </svg> 
-                                                    </a> 
+                                                    </a>  --}}
                                                 </div>
                                             </div>
                                         </div>
@@ -32,7 +32,7 @@
                                 </div>
 
                             </div>
-                        </div>
+                        </a>
                         @endforeach
                         
                     </div>
